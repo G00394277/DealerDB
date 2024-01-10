13cf7d60023cfb9c443eb4efd8c59db7e531911d
G00394277 <G00394277@atu.ie>
2024-01-10 02:04:35 +0000

    Initial commit

---
+++ src/main/java/ie/atu/dealerdb/DealerDbApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.dealerdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerDbApplication.class, args);
+    }
+
+}
